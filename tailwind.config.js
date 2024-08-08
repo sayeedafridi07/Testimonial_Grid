c9fc9c2bdb7dfffa82014770d9e36ecdbe005ecd
sayeedafridi07 <sayeedafridi07@gmail.com>
2024-08-09 01:17:20 +0530

    Completed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
